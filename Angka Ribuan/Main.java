51ddaaba9e9c3bb129b6aed66195705175c901d0
mbul48 <45551081+mbul48@users.noreply.github.com>
2021-10-03 01:16:35 +0700

    Update Main.java
    one

--- Angka Ribuan/Main.java
+++ Angka Ribuan/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Locale bahasa = new Locale("id");
-    Scanner input = new Scanner(System.in);
-    int harga = input.nextInt();
+    Scanner inp = new Scanner(System.in);
+    int harga = inp.nextInt();
     
     System.out.printf(bahasa, "Rp%,d,-", harga);
     
